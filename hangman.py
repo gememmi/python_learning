c1870da6800c5d1fc5d9f23337f51a9cd62121ee
Emily <emelizadaniels@gmail.com>
2023-08-21 16:25:22 -0400

    learning classes in Python

--- hangman.py
+++ hangman.py
@@ -4,34 +4,57 @@ import string
 
 
 def get_valid_word(words):
-    # random.choice allows you to choose a random thing from a list
     word = random.choice(words)
     while '-' in word or ' ' in word:
         word = random.choice(words)
-        # so if a word contains a space or a dash, we will keep iterating until we find one without.
-
+    
     return word.upper()
 
 def hangman():
     word = get_valid_word(words)
-    # we use the valid word that our first function found for us and then use it in this funcion
-    word_letters = set(word) 
-    # set creates an object so here we are creating an object of single letter singles from our chosen word
-    alphabet = set(string.ascii_uppercase)
-    # what the user has already guess will get moved here
-    used_letters = set()   
-    # users letter guess in uppercase
-    user_letter = input('Guess a letter: ').upper()
-    if user_letter in alphabet - used_letters:
-        used_letters.add(user_letter)
-        if user_letter in word_letters:
-            word_letters.remove(user_letter)
+    word_letters = set(word)
+    alphabet = set(string.ascii_uppercase) 
+    used_letters = set()  
+    
+    lives = 6
 
-    elif user_letter in used_letters:
-        print('You have already used that character. Please try again')
+    # getting user input
+    while len(word_letters) > 0 and lives > 0: 
+        # letters used
+        # ' '.join(['a', 'b', 'c']) --> 'a b c'
+        print( 'You have ', lives, 'lives left and you have you have used these letters: ', ''.join(used_letters))
+        # what the current word is(ie W - R D)
+        word_list = [letter if letter in used_letters else "-" for letter in word]
+        print('Current word: ', ''.join(word_list))
+        user_letter = input('Guess a letter: ').upper()
+        if user_letter in alphabet - used_letters:
+            used_letters.add(user_letter)
+            if user_letter in word_letters:
+                word_letters.remove(user_letter)
+            else:
+                lives = lives -1
+        elif user_letter in used_letters:
+            print('You have already used that character. Please try again')
+        else:
+            print("Not a valid character, try a valid character")
+
+    if lives == 0:
+        print('Sorry, you died')
     else:
-        user_letter in alphabet
-        print("Not a valid character, try a valid character")
+        print('You guessed the word', word, '!')
 
-user_input = input("Type something: ")
-print(user_input)
+hangman()
+
+
+# We first need to make sure that the randome word we are choosing is actually valid, so we create function to test that. 
+# We create a while function that will keep finding a random word until it is valid.
+# We bring that valid found word into our next funciton by calling that first function. 
+# and assiging it to a variable called word. Next we make a set, or object, of letters from the word.
+# We create an empty set where we will add all the letters that the user has already guessed
+# We create an input field for the user to guess a letter and make it uppercase
+# Then we create a condition so that if the users guess is valid and not in the used letttesr
+# then add that letter to the used letters. Then if that guess in the actual word, remove that letter from the 
+# word. If the users guess is in the used letters, send them a message. If the users guess is not in the used letters
+# and not in the alphabet, print a message that their guess is not valid. 
+# We want the use to be able to see what letters they have already guess so we can join the used_letters together using .join() which we use on an empty string. 
+# Then we create a list that will contain the letter 
